4686c3c9ca1a1edb61bcbae0885f6536bfe35302
JPechberty <j.pechberty@gmail.com>
2025-09-18 23:22:01 +0200

    Etape 2.3

---
+++ src/Entity/Module.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModuleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ModuleRepository::class)]
+class Module
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    #[Assert\NotBlank(message: "Le titre est obligatoire")]
+    #[Assert\Length(min: 5, max: 150, minMessage: "Le titre doit faire au moins {{ limit }} caractères", maxMessage: "Le titre ne peut pas faire plus de {{ limit }} caractères")]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: "La description est obligatoire")]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\Positive(message: "L'ordre doit être un nombre positif")]
+    private ?int $ordre = null;
+
+    #[ORM\Column]
+    #[Assert\Positive(message: "La durée estimée doit être un nombre positif")]
+    private ?int $dureeEstimee = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getOrdre(): ?int
+    {
+        return $this->ordre;
+    }
+
+    public function setOrdre(int $ordre): static
+    {
+        $this->ordre = $ordre;
+
+        return $this;
+    }
+
+    public function getDureeEstimee(): ?int
+    {
+        return $this->dureeEstimee;
+    }
+
+    public function setDureeEstimee(int $dureeEstimee): static
+    {
+        $this->dureeEstimee = $dureeEstimee;
+
+        return $this;
+    }
+}
